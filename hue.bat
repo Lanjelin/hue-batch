f33f9feed4a5a51e46116c57b483437e4a0f0263
Lanjelin <lanjelin.svanes@gmail.com>
2014-08-10 22:24:55 +0200

    Small bugfix

--- hue.bat
+++ hue.bat
@@ -1,7 +1,7 @@
 :: To make this script work, you need to download cURL with SSL Support, aswell as the OPENSSL dlls
 :: from http://www.paehl.com/open_source/?CURL_7.37.0 and place them in the same folder as this script.
 :: The process to obtain your secret key is explained here: http://developers.meethue.com/gettingstarted.html
-@echo off::&cls
+@echo off
 setlocal EnableDelayedExpansion
 
 :: Getting IP of Bridge
